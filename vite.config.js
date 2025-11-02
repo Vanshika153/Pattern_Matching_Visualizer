655532db3beb335f8a4b8b446ebf43ffb47e66e7
Vanshika153 <vanshika.goyal.ug24@nsut.ac.in>
2025-11-02 18:18:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ base: '/Pattern_Matching_Visualizer/',  plugins:[react()], server:{ port:5173, open:true } });
+
+export default defineConfig({ 
+  plugins: [react()], 
+  server: { 
+    port: 5173, 
+    open: true 
+  } 
+});
